5a125ca889e7c3894248526de9d69058daedbf75
Oscar Villagran <oscar.villagran@hotmail.com>
2023-06-02 19:57:44 -0500

    Add filteredCategories function for chosse a movie category

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,24 +1,17 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import getMovies from './fetchMovies';
 import '../styles/Movie.css';
-import { reserveMovie, cancelMovie } from '../redux/movie/moviesSlice';
 import Hero from '../assets/Hero.jpg';
-import PopularImg from '../assets/Popular.jpg';
-import NowPlayingImg from '../assets/NowPlaying.jpg';
-import TopRatedImg from '../assets/TopRated.jpg';
-import UpcomingImg from '../assets/Upcoming.jpg';
+import Popular from '../assets/popular.jpg';
+import Nowplaying from '../assets/nowplaying.jpg';
+import Toprated from '../assets/toprated.jpg';
+import Upcoming from '../assets/upcoming.jpg';
 
 const Home = () => {
   const dispatch = useDispatch();
-  const movies = useSelector((state) => state.movies.movies); // Access 'movies' correctly
   const status = useSelector((state) => state.movies.status); // Access 'status' correctly
-  const handleReserveMovie = (movieId) => {
-    dispatch(reserveMovie(movieId));
-  };
-  const handleCancelMovie = (movieId) => {
-    dispatch(cancelMovie(movieId));
-  };
+  const [filter, setFilter] = useState('');
 
   useEffect(() => {
     dispatch(getMovies());
@@ -32,6 +25,21 @@ const Home = () => {
     return <div>Error occurred while fetching movies.</div>;
   }
 
+  const categories = [
+    { name: 'popular', image: Popular },
+    { name: 'nowplaying', image: Nowplaying },
+    { name: 'toprated', image: Toprated },
+    { name: 'upcoming', image: Upcoming },
+  ];
+
+  const filteredCategories = categories.filter(
+    (category) => category.name.toLowerCase().includes(filter.toLowerCase()),
+  );
+
+  const handleInputChange = (event) => {
+    setFilter(event.target.value);
+  };
+
   return (
     <div>
       <div className="hero flex-column" style={{ backgroundImage: `url(${Hero})` }}>
@@ -42,47 +50,30 @@ const Home = () => {
         <input
           className="search-input"
           placeholder="search by category"
-          value=""
+          value={filter}
+          onChange={handleInputChange}
           type="text"
         />
       </div>
       <section className="movie-categories">
         <div className="grid-movies-section">
-          <div className="movie-background flex-column popular" style={{ backgroundImage: `url(${PopularImg})` }}>
-            <button type="button" className="movie-category">Popular</button>
-          </div>
-          <div className="movie-background flex-column nowplaying" style={{ backgroundImage: `url(${NowPlayingImg})` }}>
-            <button type="button" className="movie-category">Now Playing</button>
-          </div>
-          <div className="movie-background flex-column toprated" style={{ backgroundImage: `url(${TopRatedImg})` }}>
-            <button type="button" className="movie-category">Top Rated</button>
-          </div>
-          <div className="movie-background flex-column upcomming" style={{ backgroundImage: `url(${UpcomingImg})` }}>
-            <button type="button" className="movie-category">Upcoming</button>
-          </div>
+          {filteredCategories.length > 0 ? (
+            filteredCategories.map((category) => (
+              <div
+                key={category.name}
+                className={`movie-background flex-column ${category.name}`}
+                style={{ backgroundImage: `url(${category.image})` }}
+              >
+                <button type="button" className="movie-category">
+                  {category.name.charAt(0).toUpperCase() + category.name.slice(1)}
+                </button>
+              </div>
+            ))
+          ) : (
+            <div>No categories found.</div>
+          )}
         </div>
       </section>
-
-      {movies?.map((movie) => (
-        <div className="movie flex" key={movie.id}>
-          <img className="movie-img" src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt="movie_image" />
-          <div className="movie-information">
-            <h2 className="movie-name">{movie.original_title}</h2>
-            <h3 className="movie-type">{movie.overview}</h3>
-            {movie.reserved ? (
-              <p className="movie-description">
-                <button className="reserve-badge" type="button">reserved</button>
-                {movie.title}
-              </p>
-            ) : (
-              <p className="movie-description">{movie.title}</p>
-            )}
-            {!movie.reserved
-              ? <button className="reserve-btn" type="button" onClick={() => handleReserveMovie(movie.id)}>reserve Movie</button>
-              : <button className="reserve-btn cancel" type="button" onClick={() => handleCancelMovie(movie.id)}>cancel reservation</button>}
-          </div>
-        </div>
-      ))}
     </div>
   );
 };
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,27 +1,57 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import getMovies from './fetchMovies';
+import '../styles/Movie.css';
+import { reserveMovie, cancelMovie } from '../redux/movie/moviesSlice';
 
-const Profile = () => {
-  const movies = useSelector((state) => state.movies.movies);
-  const reservedMovies = movies.filter((movie) => movie.reserved === true);
+const Home = () => {
+  const dispatch = useDispatch();
+  const movies = useSelector((state) => state.movies.movies); // Access 'movies' correctly
+  const status = useSelector((state) => state.movies.status); // Access 'status' correctly
+
+  const handleReserveMovie = (movieId) => {
+    dispatch(reserveMovie(movieId));
+  };
+  const handleCancelMovie = (movieId) => {
+    dispatch(cancelMovie(movieId));
+  };
+
+  useEffect(() => {
+    dispatch(getMovies());
+  }, [dispatch]);
+
+  if (status === 'loading') {
+    return <div>Loading...</div>;
+  }
+
+  if (status === 'failed') {
+    return <div>Error occurred while fetching movies.</div>;
+  }
+
   return (
-    <div className="my-profile">
-      <div>
-        <h2>My Movies</h2>
-        <ul>
-          { reservedMovies.length > 0 ? (
-            reservedMovies.map((movie) => (
-              <li key={movie.id}>
-                {movie.original_title}
-              </li>
-            ))
-          ) : (
-            <li>no bookings</li>
-          )}
-        </ul>
-      </div>
+    <div>
+      {movies?.map((movie) => (
+        <div className="movie flex" key={movie.id}>
+          <img className="movie-img" src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt="movie_image" />
+          <div className="movie-information">
+            <h2 className="movie-name">{movie.original_title}</h2>
+            <h3 className="movie-type">{movie.overview}</h3>
+            {movie.reserved ? (
+              <p className="movie-description">
+                <button className="reserve-badge" type="button">reserved</button>
+                {movie.title}
+              </p>
+            ) : (
+              <p className="movie-description">{movie.title}</p>
+            )}
+            {!movie.reserved
+              ? <button className="reserve-btn" type="button" onClick={() => handleReserveMovie(movie.id)}>reserve Movie</button>
+              : <button className="reserve-btn cancel" type="button" onClick={() => handleCancelMovie(movie.id)}>cancel reservation</button>}
+          </div>
+        </div>
+      ))}
     </div>
   );
 };
 
-export default Profile;
+export default Home;
